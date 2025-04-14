84161c0abc4878b5c179f1ed988a757bd3a19918
pBread <4473570+pBread@users.noreply.github.com>
2025-04-14 12:13:30 -0600

    working through

---
+++ src/benchmarks/create-message.ts
@@ -0,0 +1,35 @@
+import { AIProjectsClient } from "@azure/ai-projects";
+import { DefaultAzureCredential } from "@azure/identity";
+import { AGENT_ID, AZURE_CONN_STRING } from "../env.js";
+import { createLogger } from "../logger.js";
+
+const client = AIProjectsClient.fromConnectionString(
+  AZURE_CONN_STRING,
+  new DefaultAzureCredential(),
+);
+
+export async function createMessage(threadId: string) {
+  const logger = createLogger("create-message.log");
+  logger.log(threadId);
+
+  const run = await client.agents
+    .createRun(threadId, AGENT_ID, {
+      additionalMessages: [
+        { role: "user", content: "Hello, how are you today?" },
+      ],
+    })
+    .stream();
+
+  const eventSet = new Set();
+
+  for await (const chunk of run) {
+    if (!eventSet.has(chunk.event)) {
+      logger.snapshot(chunk.event);
+      eventSet.add(chunk.event);
+    }
+
+    logger.log(chunk.event, "\n", JSON.stringify(chunk, null, 2));
+  }
+
+  logger.printSnapshots();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { toolExecution } from "./benchmarks/tool-execution.js";
+import { createMessage } from "./benchmarks/create-message.js";
 import { createThread } from "./benchmarks/create-thread.js";
+import { toolExecution } from "./benchmarks/tool-execution.js";
 
 console.log("starting");
 (async () => {
   const threadId = await createThread();
+  console.log("\n");
+  await createMessage(threadId);
   console.log("\n");
   await toolExecution(threadId);
 })();
